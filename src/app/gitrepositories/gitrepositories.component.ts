4d5600629287f6ba5bba26bbfbcbe517b0b4abc3
Essy Mwangi <sonnieessy@gmail.com>
2020-04-13 12:02:38 +0300

    Used font awesome to get icons representing all attributes

--- src/app/gitrepositories/gitrepositories.component.ts
+++ src/app/gitrepositories/gitrepositories.component.ts
@@ -1,33 +1,29 @@
-import { GituserserviceService } from './../gituserservice.service';
-import { Gitrepositories } from './../gitrepositories';
-import { Component, OnInit } from '@angular/core';
-
+import { GituserserviceService } from "./../gituserservice.service";
+import { Gitrepositories } from "./../gitrepositories";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-gitrepositories',
-  templateUrl: './gitrepositories.component.html',
-  styleUrls: ['./gitrepositories.component.css']
+  selector: "app-gitrepositories",
+  templateUrl: "./gitrepositories.component.html",
+  styleUrls: ["./gitrepositories.component.css"],
 })
 export class GitrepositoriesComponent implements OnInit {
   repo: Gitrepositories;
-  constructor( public repoService: GituserserviceService ) { }
+  constructor(public repoService: GituserserviceService) {}
 
-  repoSearch(searchName){
+  repoSearch(searchName) {
     this.repoService.getReopos(searchName).then(
-      (results)=>{
-        this.repo =this.repoService.allRepos
+      (results) => {
+        this.repo = this.repoService.allRepos;
         console.log(this.repo);
       },
-      (error)=>{
+      (error) => {
         console.log(error);
       }
     );
   }
 
   ngOnInit() {
-    this.repoSearch('EssyMwangi');
+    this.repoSearch("EssyMwangi");
   }
-
- 
-
 }
--- src/app/gitusers/gitusers.component.ts
+++ src/app/gitusers/gitusers.component.ts
@@ -1,44 +1,42 @@
-import { GituserserviceService } from './../gituserservice.service';
-import { Gitrepositories } from './../gitrepositories';
-import { Gitusers } from './../gitusers';
-import { Component, OnInit } from '@angular/core';
-
+import { GituserserviceService } from "./../gituserservice.service";
+import { Gitrepositories } from "./../gitrepositories";
+import { Gitusers } from "./../gitusers";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-gitusers',
-  templateUrl: './gitusers.component.html',
-  styleUrls: ['./gitusers.component.css']
+  selector: "app-gitusers",
+  templateUrl: "./gitusers.component.html",
+  styleUrls: ["./gitusers.component.css"],
 })
 export class GitusersComponent implements OnInit {
   user: Gitusers;
   repo: Gitrepositories;
-  constructor(public myService: GituserserviceService, private repoService: GituserserviceService) {
-  }
+  constructor(
+    public myService: GituserserviceService,
+    private repoService: GituserserviceService
+  ) {}
 
   searchs(searchName) {
     this.myService.searchUSer(searchName).then(
-      (success)=>{
+      (success) => {
         this.user = this.myService.foundUser;
       },
-      (error)=>{
-        console.log(error)
+      (error) => {
+        console.log(error);
       }
     );
-      this.repoService.getReopos(searchName).then(
-        (results)=>{
-          this.repo =this.repoService.allRepos
-          console.log(this.repo);
-        },
-        (error)=>{
-          console.log(error);
-        }
-      );
+    this.repoService.getReopos(searchName).then(
+      (results) => {
+        this.repo = this.repoService.allRepos;
+        console.log(this.repo);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
   ngOnInit() {
-    this.searchs('EssyMwangi');
+    this.searchs("EssyMwangi");
   }
-
-  
-
 }
